57a6cfe9878b687c5472f8758ece25b5096d1a1e
jeonghan-lee <qlqhqo2341@naver.com>
2021-08-06 00:15:48 +0900

    기본 체크 로직 구현

---
+++ check.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import requests as rq
+import os
+
+env = os.environ
+L7URL = env['L7URL'] if 'L7URL' in env else 'https://www.naver.coma'
+ERROR_FILE = env['ERROR_FILE'] if 'ERROR_FILE' in env else 'error.txt'
+
+ERROR_MESSAGE = 'unknown error'
+
+try:
+    result = rq.get(L7URL)
+    if result.status_code == 200:
+        exit(0)
+    else:
+        ERROR_MESSAGE = f"returned code : {result.status_code}"
+except Exception as ex:
+    ERROR_MESSAGE = str(ex)
+    pass
+
+with open(ERROR_FILE, "wt") as wf:
+    print(ERROR_MESSAGE, file=wf)
+
+exit(1)
